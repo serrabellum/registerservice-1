4df60196610baf9acff559299f2d92ea9b6168a6
jokinsey <jokinsey@email.uark.edu>
2017-04-09 16:55:19 -0500

    More in Transaction
    - added more JSON handling

--- src/main/java/edu/uark/models/api/Transaction.java
+++ src/main/java/edu/uark/models/api/Transaction.java
@@ -1,33 +1,80 @@
 package edu.uark.models.api;
 
+import java.sql.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.UUID;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 
 
 public class Transaction {
-	private Map <String,Integer> ProductsSold;
-	private UUID transactionID;
-	private float totalCost;
-	private String date;
-	private String employeeID;
-	private String transactionType;
-	
-	
-	public Transaction(JSONObject values)
-	{
-		setMap(values.getJSONArray("transaction"));
+	public enum Transactiontype{
+		RETURN,SALE;
 	}
 	
-	public void setMap(JSONArray ProductData){
-		ProductsSold = new HashMap();
+	private Map <String, Double> ProductsSold; //added
+	private UUID transactionID; //added
+	private float totalCost; //added
+	private String date; //added
+	private String employeeID; //added
+	private  Transactiontype transactionType;
+	
+	
+	public Transaction(JSONObject values) throws JSONException
+	{
+		setTransactionType(values.getString("transactionType"));
+		setDate(values.getString("date"));
+		setEmployeeID(values.getString("employeeID"));
+		setTransactionID(UUID.fromString(values.getString("transactionID")));
+		setMap(values.getJSONArray("transaction"));
+		setTotalCost();
+		
+	}
+	
+	private void setMap(JSONArray ProductData){
+		String product;
+		double amount;
+		ProductsSold = new HashMap<>();
 		for(int i = 0; i < ProductData.length(); i++){
-			
+			product = ProductData.getJSONObject(i).getString("productID");
+			amount = ProductData.getJSONObject(i).getDouble("price");
+			ProductsSold.put(product, amount);
 		}
 	}
+	
+	private void setTransactionType(String transaction){
+		if(transaction.equals("Return")){
+			this.transactionType = Transactiontype.RETURN;
+		} else {
+			this.transactionType = Transactiontype.SALE;
+		}
+	}
+	
+	private void setDate(String date) {
+		this.date = date;
+	}
+	
+	private void setTotalCost(){
+		float sum = 0;
+		for(String key: ProductsSold.keySet()){
+			sum += ProductsSold.get(key);
+		}
+		this.totalCost = sum;
+	}
+	
+	private void setEmployeeID(String employeeID){
+		this.employeeID = employeeID;
+	}
+	
+	private void setTransactionID(UUID transactionID){
+		this.transactionID = transactionID;
+	}
+	
 }
